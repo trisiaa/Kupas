3b38ebb063a44eaed11069833b9ca1869b8f6fb8
trisiaa <ninis2408@gmail.com>
2026-04-14 19:28:20 +0700

    Fungsi Button Control Masak

--- Assets/Script/ControlGameMasak.cs
+++ Assets/Script/ControlGameMasak.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 
 public class ControlGameMasak : MonoBehaviour
 {
+    public Animator piringMakanan;
+
+    public GameObject nasi;
+    public GameObject ikan;
+    public GameObject ayam;
+    public GameObject tempe;
+    public GameObject sayur;
+    public GameObject serundeng;
+    public GameObject sambal;
+
     public void ButtonAnimation(Animator animatorButton)
     {
         animatorButton.Play("ButtonPressed", 0, 0f);
+    }
+
+    // FUNCTION UMUM
+    public void TambahMakanan(GameObject makanan)
+    {
+        if (!makanan.activeInHierarchy)
+        {
+            makanan.SetActive(true);
+            ButtonAnimation(piringMakanan);
+        }
     }
 }
